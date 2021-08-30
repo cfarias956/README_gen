29017e6de32f3c60dffdc47e735de123feaa6e7f
JJP_512 <jay.perez78@yahoo.com>
2021-08-30 14:01:02 -0500

    added questions to index.js

--- index.js
+++ index.js
@@ -1,7 +1,112 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log("Please enter title of your project!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of your project.",
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log("Please enter a description of your project!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter installation instructions for your project.",
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log("Please enter installation instructions for your project!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please provide instructions/examples for use.",
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log("Please enter usage information!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please provide contributing instructions.",
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log("Please provide contributing instructions!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Please provide instructions on how to test your application.",
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log("Please provide instructions on how to test your application!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log("Please enter your GitHub username!");
+                return false;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?",
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log("Please enter your email!");
+                return false;
+            }
+        }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
